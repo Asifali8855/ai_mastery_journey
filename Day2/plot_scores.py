adc760e014acba9a66960fa5e8ded5b28a63b8e7
Asifali8855 <132221249+Asifali8855@users.noreply.github.com>
2025-04-17 06:40:17 +0500

    Update plot_scores.py

--- Day2/plot_scores.py
+++ Day2/plot_scores.py
@@ -1,1 +1,17 @@
+import matplotlib.pyplot as plt
+
+students = ["Ali", "Sara", "Asif", "Aisha", "John"]
+scores = [85, 90, 78, 92, 88]
+
+plt.figure(figsize=(8, 5))
+plt.bar(students, scores, color='skyblue')
+plt.xlabel("Students")
+plt.ylabel("Scores")
+plt.title("Student Test Scores")
+plt.grid(True)
+plt.tight_layout()
+
+# Save and Show
+plt.savefig("Day2/images/plot_output.png")  # Save as image
+plt.show()  # Open window (if supported)
 
